bca3c4fe1ff4cc2ed034644af35e07a0451c3a10
Floriane Diem <florianediem@gmail.com>
2026-01-21 18:43:51 -0500

    reset password feature added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :registerable,
          :validatable,
+         :recoverable,
          :jwt_authenticatable,
          jwt_revocation_strategy: self
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_21_233849) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_21_234212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,9 +40,12 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_21_233849) do
     t.datetime "updated_at", null: false
     t.string "jti"
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["phone"], name: "index_users_on_phone", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vehicles", force: :cascade do |t|
