e41ff08294a908a833fee95bea0d2974446bf1aa
Floriane Diem <florianediem@gmail.com>
2026-01-21 19:10:17 -0500

    reset password feature removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :registerable,
          :validatable,
-         :recoverable,
          :jwt_authenticatable,
          jwt_revocation_strategy: self
 
